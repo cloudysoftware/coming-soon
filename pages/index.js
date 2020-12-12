290c9dc3614c0826cacd2d8e0cc5ffc5947de752
James Akrigg <james@cloudysoftware.co.uk>
2020-12-12 06:18:54 +0000

    updated to cray colours

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div className="flex items-center justify-center w-full h-screen text-white">
-      <div className="absolute bottom-0 left-0 flex items-center justify-center w-full h-full bg-gradient-to-r from-purple-700 to-pink-700">
+      <div className="absolute bottom-0 left-0 flex items-center justify-center w-full h-full bg-gradient-to-b from-gray-700 to-gray-900">
         <div className="px-8 py-4 text-center uppercase border-2 border-white cursor-default">
           Comming soon
         </div>
