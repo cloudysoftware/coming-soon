285eb4e33468c714d15661a86083ec74cf1f4f47
James Akrigg <james@cloudysoftware.co.uk>
2020-12-12 06:49:15 +0000

    spelling fix

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex items-center justify-center w-full h-screen text-white">
       <div className="absolute bottom-0 left-0 flex items-center justify-center w-full h-full bg-gradient-to-b from-gray-700 to-gray-900">
         <div className="px-8 py-4 text-center uppercase border-2 border-white cursor-default">
-          Comming soon
+          Coming soon
         </div>
       </div>
     </div>
